aad965b246a7991f5037bbc40ef3bb557f55ab40
Rayyan Uddin <rayyanuddin007@gmail.com>
2025-02-03 16:30:13 +0500

    change test path

--- test/unit/RaffleTest.t.sol
+++ test/unit/RaffleTest.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.19;
 
-import {Test} from "forge-std/Test.sol";
+import {Test} from "lib/forge-std/src/Test.sol";
 import {DeployRaffle} from "script/DeployRaffle.s.sol";
 import {Raffle} from "src/Raffle.sol";
 import {HelperConfig} from "script/HelperConfig.s.sol";
